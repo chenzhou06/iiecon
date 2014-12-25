d036e31ac919691f7d3224645e803327ec28e8c5
陈洲 <czwork@outlook.com>
2014-12-25 22:08:19 +0800

    mysql -> sqlite

--- app/models.py
+++ app/models.py
@@ -1,12 +1,30 @@
 from flask.ext.login import UserMixin
 from . import db, login_manager
 
+
+class Permission:
+	FOLLOW = 0x01
+	COMMENT = 0x02
+	WRITE_ARTICLES = 0x04
+	MODERATE_COMMENTS = 0x08
+	ADMINISTER = 0x80
+
+
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+
+class Role(db.Model):
+	__tablename__ = "roles"
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(64), unique=True)
+	default = db.Column(db.Boolean, default=False, index=True)
+	permission = db.Column(db.Integer)
+	users = db.relationship("User", backref="role", lazy="dynamic")
 
 @login_manager.user_loader
 def load_user(user_id):
