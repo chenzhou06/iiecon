26614cc8f3f2d1bee679c77dfc4c3ed12fe5a1b8
陈洲 <czwork@outlook.com>
2014-12-26 14:04:28 +0800

    email register to be confiremed, username validator to be corrected.

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,8 @@
 from flask.ext.wtf import Form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Required, Email, Length
+from wtforms.validators import Required, Email, Length, Regexp, EqualTo
+from wtforms import ValidationError
+from ..models import User
 
 class LoginForm(Form):
     email = StringField("电子邮件", validators=[Required(), Length(1,64),
@@ -8,3 +10,24 @@ class LoginForm(Form):
     password = PasswordField("密码", validators=[Required()])
     remember_me = BooleanField("保持登录")
     submit = SubmitField("登录")
+
+
+class RegistrationForm(Form):
+    email = StringField("邮箱", validators=[Required(), Length(1,64),
+                                            Email()])
+    username = StringField("用户名", validators=[
+        Required(), Length(1.64), Regexp("[^[A-Za-z][A-Za-z0-9_.]*$", 0,
+                                         "用户名只能包含字母、数字、小数点和下划线 "
+                                         )])
+    password = PasswordField("密码", validators=[
+        Required(), EqualTo("password2", message="密码不一致")])
+    password2 = PasswordField("重复密码", validators=[Required()])
+    submit = SubmitField("注册")
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError("邮箱已注册")
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError("用户名已注册")
--- app/auth/views.py
+++ app/auth/views.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, request, url_for, flash
 from flask.ext.login import login_user, logout_user, login_required
 from . import auth
 from ..models import User
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 
 @auth.route("/login", methods=["GET", "POST"])
 def login():
@@ -22,3 +22,16 @@ def logout():
     logout_user()
     flash("注销成功")
     return redirect(url_for("xtu.index"))
+
+
+@auth.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        flash("注册成功，您现在可以登录了。")
+        return redirect(url_for("auth.login"))
+    return render_template("auth/register.html", form=form)
